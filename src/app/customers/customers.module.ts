32e6958f899284442a1fe8b17f4da42985c2d4d0
vjpal3 <vrishalipal@yahoo.com>
2020-01-31 19:45:07 -0500

    Add a child component

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { CustomersComponent } from "./customers.component";
+import { CustomersListComponent } from "./customers-list/customers-list.component";
 
 @NgModule({
-  declarations: [CustomersComponent],
+  declarations: [CustomersComponent, CustomersListComponent],
   imports: [CommonModule],
   exports: [CustomersComponent]
 })
