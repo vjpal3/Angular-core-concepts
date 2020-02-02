5832781137fe08ef5dfbd20cb55a0fd028ce90a5
vjpal3 <vrishalipal@yahoo.com>
2020-02-02 15:16:56 -0500

     Inject a Service into a Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 
+import { CoreModule } from "./core/core.module";
 import { CustomersModule } from "./customers/customers.module";
 import { SharedModule } from "./shared/shared.module";
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CustomersModule, SharedModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    AppRoutingModule,
+    CustomersModule,
+    SharedModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
