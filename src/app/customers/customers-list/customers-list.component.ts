3e0bab542f114828c27f099d7a1369fced953440
vjpal3 <vrishalipal@yahoo.com>
2020-02-01 22:02:03 -0500

    two-way data binding

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -25,44 +25,28 @@ export class CustomersListComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {
-    // this.filteredCustomers = [
-    //   {
-    //     id: 1,
-    //     name: "john Doe",
-    //     city: "Phoenix",
-    //     orderTotal: 9.99,
-    //     customerSince: new Date(2014, 7, 10)
-    //   },
-    //   {
-    //     id: 2,
-    //     name: "Jane Doe",
-    //     city: "Chandler",
-    //     orderTotal: 19.99,
-    //     customerSince: new Date(2017, 2, 22)
-    //   },
-    //   {
-    //     id: 3,
-    //     name: "Michelle Thomas",
-    //     city: "Seattle",
-    //     orderTotal: 99.99,
-    //     customerSince: new Date(2002, 10, 31)
-    //   },
-    //   {
-    //     id: 4,
-    //     name: "Jim Thomas",
-    //     city: "New York",
-    //     orderTotal: 599.99,
-    //     customerSince: new Date(2002, 10, 31)
-    //   }
-    // ];
-  }
+  ngOnInit() {}
 
   calculateOrders() {
     this.customersOrderTotal = 0;
     this.filteredCustomers.forEach((customer: ICustomer) => {
       this.customersOrderTotal += customer.orderTotal;
     });
+  }
+
+  filter(data: string) {
+    if (data) {
+      this.filteredCustomers = this.customers.filter((cust: ICustomer) => {
+        return (
+          cust.name.toLowerCase().indexOf(data.toLowerCase()) > -1 ||
+          cust.city.toLowerCase().indexOf(data.toLowerCase()) > -1 ||
+          cust.orderTotal.toString().indexOf(data.toLowerCase()) > -1
+        );
+      });
+    } else {
+      this.filteredCustomers = this.customers;
+    }
+    this.calculateOrders();
   }
 
   sort(prop: string) {
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 import { CustomersComponent } from "./customers.component";
 import { CustomersListComponent } from "./customers-list/customers-list.component";
@@ -12,7 +13,7 @@ import { SharedModule } from "../shared/shared.module";
     CustomersListComponent,
     FilterTextboxComponent
   ],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, FormsModule],
   exports: [CustomersComponent, FilterTextboxComponent]
 })
 export class CustomersModule {}
